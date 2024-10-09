4d559aa85d5660019df12b59ae20385b61642221
Arthurs101 <arg21527@uvg.edu.gt>
2024-10-09 12:13:44 -0600

    added support for both testing

--- AutoTester.sh
+++ AutoTester.sh
@@ -1,13 +1,22 @@
 text="Esto es una prueba de proyecto 2"
 keyword="una prueba"
-allkeys=(1528012 23451232 36028797018963969 45035996273704960 15836833854489655)
+allkeys=(1528012 23451232 36028797018963969)
 parallel_times="parallel_times.txt"  # Output file
+sec_times="secuential_times.txt"
 cd "build"
 touch $parallel_times
+touch $sec_times
 for key in ${allkeys[@]}; do
-    echo "Running with key: $key" >> "$parallel_times" 
+    echo "Parallel Running with key: $key" >> "$parallel_times" 
     ./xor_cipher "$text" $key
     mpirun -np 4 ./bruteforce_mpi cipherC.bin  "$keyword" >> "$parallel_times"
     echo "" >> "$parallel_times"
+done
+
+for key in ${allkeys[@]}; do
+    echo "Secuential Running with key: $key" >> "$sec_times" 
+    ./xor_cipher "$text" $key
+    ./bruteforce_sequential cipherC.bin "$keyword" >> "$sec_times"
+    echo "" >> "$sec_times"
 done
 cd ..
